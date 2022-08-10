c91f73eeb7fdf391abf911e1c777df5360406287
SHIMIL T <shimiltharayil007@gmail.com>
2022-08-10 23:35:54 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,45 +1,32 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+let  items = [];
 app.set('view engine', 'ejs');
 
-app.get("/",function(req,res){
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
- let today = new Date();
- let currentDay = today.getDay();
- let day = "";
-
- switch (currentDay) {
-             case 0:
-              day = "Sunday";
-            break;
-            case 1:
-            day = "Monday";       
-            break;
-            case 2:
-                day = "Tuesday";       
-            break;
-            case 3:
-                day = "Wednesday";     
-            break;
-            case 4:
-                day = "Thursday";
-            break;
-            case 5:
-                day = "Friday";
-            break;
-            case 6:
-                day = "Saturday";
-            break; 
-            case 7:
-                day = "Sunday";
-            break; 
-    default:
-        break;
- }
 
 
 
 app.listen(3000,function(){
     console.log("Server started");
+    app.get("/",function(req,res){
+        app.post('/', (req, res) => {
+            let item = req.body.addItem;
+             items.push(item);
+            console.log("added",item);
+            res.redirect("/");
+            
+        });
+        let today = new Date();
+        let options ={
+            weekday:"long",
+            day : "numeric",
+            month:"long"
+        }
+        let day=today.toLocaleDateString("en-US",options);
+        res.render("list",{ kindOfDay:day, newItems:items});
+});
 });
